865a567105f37457657cccd916cb67285e9ea518
wwww-teddy <yddet.www@gmail.com>
2024-01-17 11:34:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
+import os
 from SpotAPI import *
 from YtAPI import *
 
 def run():
-    #playlist_url = input("Enter your spotify playlist's URL: ")
-    
-    yt_set_credentials()
-    track_list = get_playlist("https://open.spotify.com/playlist/0Gc1CHlpYK0KuGrt8nPK2V?si=a4bc2dc9a7d7469f")
-    new_playlist_id = create_playlist()
-
-    for index, track in enumerate(track_list):
-        title = track['title']
-        artists = " ".join(track['artists'])
-    
-        key_word = f"{title} {artists}"
-        video_id = search_video(key_word)
+    # YOUTUBE QUOTA LIMIT RUINING EVERYTHING, SO WE MAKING A QUEUE
+    if(os.stat("queue.txt").st_size == 0):
+        print("is empty")
+        # playlist_url = input("Enter your spotify playlist's URL: ")
+        track_list = get_playlist("https://open.spotify.com/playlist/0Gc1CHlpYK0KuGrt8nPK2V?si=a4bc2dc9a7d7469f")
         
-        print(f"{key_word} {video_id}")
-        insert_vid_to_playlist(playlist_id=new_playlist_id, video_id=video_id)
+        for index, track in enumerate(track_list):
+            title = track["title"]
+            artists = " ".join(track["artists"])
+    
+            key_word = f"{title} {artists}"
+            print(key_word)
+        
+            with open("queue.txt", "a", encoding="utf-8") as file:
+                file.write(f"{key_word}\n")
+    else:
+        print("isnt empty")
+    
+    
+    
+    
+    
+    # yt_set_credentials()
+    # new_playlist_id = create_playlist()
+
+    
+    
+        # video_id = search_video(key_word)
+        
+        # print(f"{key_word} {video_id}")
+        # insert_vid_to_playlist(playlist_id=new_playlist_id, video_id=video_id)
     
 if __name__ == '__main__':
     run()
