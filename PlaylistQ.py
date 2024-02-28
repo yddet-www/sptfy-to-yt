1ea7c9741dde59c7f14cc6f9a9eeeae872b250b3
wwww-teddy <yddet.www@gmail.com>
2024-02-28 00:07:40 -0600

    Getters dan Setters telah diimplimentasikan

--- PlaylistQ.py
+++ PlaylistQ.py
@@ -2,19 +2,22 @@ import os
 
 class Playlist:
 
-    def __init__(self, src_playlist, trgt_playlist, lst: list):
+    def __init__(self, src_playlist, trgt_playlist, lst):
         self.src_playlist = src_playlist # spotify playlist
         self.trgt_playlist = trgt_playlist # youtube playlsit
-        self.trackList = lst
-        
+        self.tracks = lst
+
     def get_src(self):
         return self.src_playlist
     
     def get_trgt(self):
         return self.trgt_playlist
     
-    def tracks_list(self):
-        return self.trackList
+    def set_tracks(self, lst):
+        self.tracks = lst
+    
+    def get_tracks(self):
+        return self.tracks
     
     def tracks_num(self):
-        return len(self.trackList)
+        return len(self.tracks)
