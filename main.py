9b6526c36a867221b07ed343489dc6739c69f047
wwww-teddy <yddet.www@gmail.com>
2024-02-28 23:18:12 -0600

    Mengimplementasikan proses aplikasi

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from datetime import datetime, date, time, timedelta
 # Track the YT API limits
 reset_time = None
 quota = None
+queue = []
 
 # Appened input onto queue text file
 def append_to_file(line):
@@ -19,6 +20,12 @@ def read_lines():
     file = open("queue.txt", "r", encoding="utf-8")
     return file.readlines()   
 
+def is_queue():
+    if(len(queue) > 0):
+        return True
+    else:
+        return False
+
 def run():
     # YOUTUBE QUOTA LIMIT RUINING EVERYTHING, SO WE MAKING A QUEUE
     
@@ -27,7 +34,7 @@ def run():
         print("is empty")
         
         # playlist_url = input("Enter your spotify playlist's URL: ")
-        track_list = get_playlist("https://open.spotify.com/playlist/0Gc1CHlpYK0KuGrt8nPK2V?si=a4bc2dc9a7d7469f")
+        track_list = get_playlist(get_playlist_ID("https://open.spotify.com/playlist/0Gc1CHlpYK0KuGrt8nPK2V?si=a4bc2dc9a7d7469f"))
         
         for index, track in enumerate(track_list):
             title = track["title"]
@@ -87,32 +94,61 @@ def run_v2():
         reset_time = datetime.combine(today.date(), time(2, 0, 0)) + timedelta(days=1)
         
     start_p = txt.tell() # pointer to 2nd line
-    
+        
     if(not txt.readline().strip()):
-        print("WEH KOSONG")
+        print("Antrian mu kosong.")
         
-        playlist_url = "https://open.spotify.com/playlist/0Gc1CHlpYK0KuGrt8nPK2V?si=a4bc2dc9a7d7469f"
-        
-        tracks = []
-        src_ID = get_playlist_ID(playlist_url)
-        
-        for index, track in enumerate(get_playlist(playlist_url)):
-            title = track["title"]
-            artists = " ".join(track["artists"])
-    
-            key_word = f"{title} {artists}"
-            tracks.append(key_word)
-            
-        print(tracks)
     else:
-        print("WADUH ISI")
+        print("Antrian mu ada isi.")
         txt.seek(start_p) # reposition pointer to 2nd line
-        
-        queue = txt.readlines()
-        print(queue)   
-        print(len(queue))    
+        saved = txt.readlines()
+        print(saved)
+        print(len(saved))
     
     txt.close()
+        
+    app_flag = True
+    
+    while(app_flag):          
+        menu = """==============================
+Choose the following options:
+1) Add to queue
+2) Process queue
+3) Check quota left
+4) Check next reset
+5) Exit
+"""
+        response = input(menu)
+        
+        match response:
+            case "1":
+                url = input("Enter your Spotify playlist link:\n")
+                
+                src = get_playlist_ID(url)
+                tracks = []
+                trgt = None
+                
+                for index, track in enumerate(get_playlist(src)):
+                    title = track["title"]
+                    artists = " ".join(track["artists"])
+    
+                    key_word = f"{title} {artists}"
+                    tracks.append(key_word)
+                
+                print("Give the Youtube playlist link to store the result in (leave blank to create a new one:")
+                
+            case "2":
+                print("You chose option 2")
+                
+            case "3":
+                print(f"You can transfer {quota} songs")
+                
+            case "4":
+                print(f"Next quota reset is at {reset_time}")
+                
+            case default:
+                app_flag = False # end session
+
 
 if __name__ == "__main__":
     # run()
