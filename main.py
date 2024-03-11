7f098048a3a26e5a184caa77980bbc9a2d32875c
wwww-teddy <yddet.www@gmail.com>
2024-03-10 23:06:53 -0500

    Fungsi penyimpanan dan pengingatan sesi telah diterapkan

--- main.py
+++ main.py
@@ -10,21 +10,9 @@ reset_time = None
 quota = 0        # Limit of how many tracks that can be processed
 queue = []
 
-# Appened input onto queue text file
-def append_to_file(line):
-    file = open("queue.txt", "a", encoding="utf-8")
-    file.write(line + "\n")
-    file.close()
-    
 
-# Return a list of all lines in the queue text file
-def read_lines():
-    file = open("queue.txt", "r", encoding="utf-8")
-    return file.readlines()   
-
-
-def process_q(playlist_obj):
-    global quota
+def process_q(playlist_obj, index): 
+    global quota, queue
     
     track_list = playlist_obj.get_tracks()
     yt_playlist = playlist_obj.get_src()
@@ -33,37 +21,11 @@ def process_q(playlist_obj):
         vid_id = search_video(track_list.pop(0))
         insert_vid_to_playlist(yt_playlist, vid_id)
         quota -= 1
-
-
-def run():
-    # YOUTUBE QUOTA LIMIT RUINING EVERYTHING, SO WE MAKING A QUEUE
+        track_list.pop(0)
     
-    
-    if(os.stat("queue.txt").st_size == 0):
-        print("is empty")
-        
-        # playlist_url = input("Enter your spotify playlist's URL: ")
-        track_list = get_playlist(get_playlist_ID("https://open.spotify.com/playlist/0Gc1CHlpYK0KuGrt8nPK2V?si=a4bc2dc9a7d7469f"))
-        
-        for index, track in enumerate(track_list):
-            title = track["title"]
-            artists = " ".join(track["artists"])
-    
-            key_word = f"{title} {artists}"
-        
-            append_to_file(key_word)
-    else:
-        print("isnt empty")
-
-    # yt_set_credentials()
-    # new_playlist_id = create_playlist()
-
-    
-
-        # video_id = search_video(key_word)
-        
-        # print(f"{key_word} {video_id}")
-        # insert_vid_to_playlist(playlist_id=new_playlist_id, video_id=video_id)
+    if not track_list:
+        del queue[index]    # checks whether all tracks are processed
+                            # if yes, remove object from queue
 
 
 # TESTING PURPOSES
@@ -83,39 +45,34 @@ def test():
     print(playlist.toString())
     
     
-def run_v2():
+def run():
     global reset_time, quota, queue
+    yt_set_credentials()
     
-    # First line of text file strictly for quota info
+    with open("queue.txt", "r", encoding="utf-8") as txt:               # UTF-8 is important btw
+        session = [i.strip("\n").split(";") for i in txt.readlines()]   # Holds a list of lists
+                                                                        # 1st index contains the quota data in form [quota, date]
+                                                                        # the rest are saved playlist object arguments in form [src, trgt, ... tracks]
     
-    # with open("queue.txt", "r+") as txt:
-    #     quota_data = txt.readline().split(";")
-    
-    txt = open("queue.txt", "r+") # DONT FORGET TO CLOSE
-    quota_data = txt.readline().split(";")
-    
-    quota = quota_data[0]
-    reset_time = datetime.fromisoformat(quota_data[1].strip("\n"))
+    quota_data = session[0]
+    quota = int(quota_data[0])
+    reset_time = datetime.fromisoformat(quota_data[1])
     today = datetime.now()
     
-    # Reset quota count every 02:00 AM CST
-    if(today > reset_time):
+    if(today > reset_time): # Reseting quota counter
         quota = 50
         reset_time = datetime.combine(today.date(), time(2, 0, 0)) + timedelta(days=1)
         
-    start_p = txt.tell() # pointer to 2nd line
-        
-    if(not txt.readline().strip()):
-        print("Antrian mu kosong.")
-        
+    if(len(session) == 1):
+        print("Your queue is empty.")
     else:
-        print("Antrian mu ada isi.")
-        txt.seek(start_p) # reposition pointer to 2nd line
-        saved = txt.readlines()
-        print(saved)
-        print(len(saved))
-    
-    txt.close()
+        print(f"You have {len(session) - 1} items in your queue.")
+        
+        for i in range(1, len(session)):
+            src = session[i].pop(0)
+            trgt = session[i].pop(0)
+            playlist = Playlist(src, trgt, session[i])
+            queue.append(playlist)
         
     app_flag = True
     
@@ -138,8 +95,8 @@ Choose the following options:
                 url = input("Enter your Spotify playlist link:\n")
                 
                 src = get_playlist_ID(url)
+                trgt = create_playlist()
                 tracks = []
-                trgt = "PL4kxuDsb-ujr231Wim7vsAvOeZVxSGWLk" # later change to create_playlist()
                 
                 for index, track in enumerate(get_playlist(src)):
                     title = track["title"]
@@ -155,15 +112,19 @@ Choose the following options:
                 queue_index = len(queue)
                 print(f"You have {queue_index} items in queue")
                 
-                if(queue_index > 1):
+                if(quota == 0):
+                    print(f"You have run out of quota. Please check back at {reset_time}")    
+                
+                elif(queue_index > 1):
                     index = input(f"Which item (1 - {queue_index}) would you like to process? (enter E to exit)\n")
                     if(index.isdigit() and int(index) > 0 and int(index) <= queue_index):
-                        process_q(queue[int(index) - 1])
+                        process_q(queue[int(index) - 1], int(index) - 1)
+                        
                 
                 elif(queue_index == 1):
                     index = input(f"Would you like to process the entry? (Y/N)\n")
                     if(index == "Y" or index == "y"):
-                        process_q(queue[0])
+                        process_q(queue[0], 0)
                 
                 else:
                     print("Your queue is empty. Please add a new entry before processing.")
@@ -212,9 +173,18 @@ Choose the following options:
                 
             case default:
                 app_flag = False # end session
+                
+    # SAVE SESSION
+    with open("queue.txt", "w", encoding="utf-8") as txt: # UTF-8 is important btw
+        new_quota = f"{quota};{reset_time}\n"
+        session = [new_quota]
+        
+        while queue:
+            session.append(queue.pop(0).toString() + "\n")
+            
+        txt.writelines(session)
 
 
 if __name__ == "__main__":
-    # run()
+    run()
     # test()
-    run_v2()
